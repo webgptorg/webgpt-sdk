b4249bab5836081b68c059c696eef91379f48d1d
Pavol Hejny <me@pavolhejny.com>
2024-04-12 19:22:22 +0200

    Add @promptbook/core and @promptbook/types dependencies

--- src/WebgptSdk.ts
+++ src/WebgptSdk.ts
@@ -1,9 +1,20 @@
+import type { TaskProgress } from '@promptbook/types';
+
 export class WebgptSdk {
     constructor() {
         console.log('WebgptSdk constructor');
     }
+
+    public async createAssignment(
+        idea: string,
+        onProgress: (taskProgress: TaskProgress) => void,
+    ): Promise<{ assignment: string }> {
+        onProgress;
+        // TODO: !!! Better mock
+        return { assignment: idea };
+    }
 }
 
 /**
- * Annotate all
+ * TODO: !!! Annotate all
  */
