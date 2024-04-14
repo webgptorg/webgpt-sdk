bcaf273dbea69267e263cdfd17aa5073bfeec3c4
Pavol Hejny <me@pavolhejny.com>
2024-04-14 17:40:04 +0200

    Try to fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default [
-    {
-        input: `./src/index.ts`,
-        output: [
-            {
-                file: `./dist/umd/index.umd.js`,
-                name: `webgpt-sdk`,
-                format: 'umd',
-                sourcemap: true,
-            },
-            {
-                file: `./dist/esm/index.es.js`,
-                format: 'es',
-                sourcemap: true,
-            },
-        ],
-        plugins: [typescript({ tsconfig: './tsconfig.json' })],
-    },
-];
+export default {
+    input: `./src/index.ts`,
+    output: [
+        {
+            file: `./dist/umd/index.umd.js`,
+            name: `webgpt-sdk`,
+            format: 'umd',
+            sourcemap: true,
+        },
+        {
+            file: `./dist/esm/index.es.js`,
+            format: 'es',
+            sourcemap: true,
+        },
+    ],
+    plugins: [typescript({ tsconfig: './tsconfig.json' })],
+};
