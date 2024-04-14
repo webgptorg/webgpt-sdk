cf946549aded669205332c3a8dd540a5a7d70f1a
Pavol Hejny <me@pavolhejny.com>
2024-04-14 18:05:29 +0200

    Export tasks interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
+import type { MakeAssignmentOptions, MakeAssignmentProgress, MakeAssignmentResult } from './types/MakeAssignment';
+import type { MakeWebsiteOptions, MakeWebsiteProgress, MakeWebsiteResult } from './types/MakeWebsite';
 import type { SdkSocket_Error } from './types/socket/SdkSocket_Error';
 import type { SdkSocket_Progress } from './types/socket/SdkSocket_Progress';
 import type { SdkSocket_Request } from './types/socket/SdkSocket_Request';
 import type { SdkSocket_Response } from './types/socket/SdkSocket_Response';
 import { WebgptSdk } from './WebgptSdk';
 
-export { SdkSocket_Error, SdkSocket_Progress, SdkSocket_Request, SdkSocket_Response, WebgptSdk };
+export {
+    MakeAssignmentOptions,
+    MakeAssignmentProgress,
+    MakeAssignmentResult,
+    MakeWebsiteOptions,
+    MakeWebsiteProgress,
+    MakeWebsiteResult,
+    SdkSocket_Error,
+    SdkSocket_Progress,
+    SdkSocket_Request,
+    SdkSocket_Response,
+    WebgptSdk,
+};
