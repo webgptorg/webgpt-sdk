f5dc266aa10095bf7fc1a2bd485a7d3183c354c6
Pavol Hejny <me@pavolhejny.com>
2024-04-14 17:40:46 +0200

    Export types for socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { /* type */ SdkSocket_Error } from './types/socket/SdkSocket_Error';
-import { /* type */ SdkSocket_Progress } from './types/socket/SdkSocket_Progress';
-import { /* type */ SdkSocket_Request } from './types/socket/SdkSocket_Request';
-import { /* type */ SdkSocket_Response } from './types/socket/SdkSocket_Response';
+import type { SdkSocket_Error } from './types/socket/SdkSocket_Error';
+import type { SdkSocket_Progress } from './types/socket/SdkSocket_Progress';
+import type { SdkSocket_Request } from './types/socket/SdkSocket_Request';
+import type { SdkSocket_Response } from './types/socket/SdkSocket_Response';
 import { WebgptSdk } from './WebgptSdk';
 
 export { SdkSocket_Error, SdkSocket_Progress, SdkSocket_Request, SdkSocket_Response, WebgptSdk };
