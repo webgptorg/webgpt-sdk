21cd8f3abf0ed3b736a214ce0fe05fd863af673a
Pavol Hejny <me@pavolhejny.com>
2024-04-18 16:57:27 +0200

    testing the WebgptSdk

--- src/WebgptSdk.test.ts
+++ src/WebgptSdk.test.ts
@@ -2,14 +2,28 @@ import { describe, expect, it } from '@jest/globals';
 import { WebgptSdk } from './WebgptSdk';
 
 describe('how WebgptSdk works', () => {
-    it('is constructable', () => {
+    it('is constructable', () =>
         expect(
             () =>
                 new WebgptSdk({
                     apiKey: 'webgpt-sample-token',
                 }),
-        ).not.toThrow();
-    });
+        ).not.toThrow());
+
+    it('can make assignment', () =>
+        expect(async () => {
+            const webgpt = new WebgptSdk({
+                apiKey: 'webgpt-sample-token', // <- Note: Testing with a sample token, which will work as expected but creates dummy assignments
+            });
+
+            const task = await webgpt.makeAssignment({ idea: `Krokodýlí Zoo`, language: `cs` });
+
+            const { assignment } = await task.asPromise();
+
+            if (assignment.length === 0) {
+                throw new Error('Assignment is empty');
+            }
+        }).resolves.not.toThrow());
 });
 
 /**
