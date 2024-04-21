114cabd1a1160ffc81f983ddb094b01cb9e0e385
Pavol Hejny <me@pavolhejny.com>
2024-04-21 20:48:33 +0200

    Revert "Temporary remove test (to be reverted)"
    This reverts commit fab4dfb77aee7d0053979f0aaa0dfede6a75c90d.

--- src/WebgptSdk.test.ts
+++ src/WebgptSdk.test.ts
@@ -9,6 +9,21 @@ describe('how WebgptSdk works', () => {
                     apiKey: 'webgpt-sample-token',
                 }),
         ).not.toThrow());
+
+    it('can make assignment', () =>
+        expect(async () => {
+            const webgpt = new WebgptSdk({
+                apiKey: 'webgpt-sample-token', // <- Note: Testing with a sample token, which will work as expected but creates dummy assignments
+            });
+
+            const task = await webgpt.makeAssignment({ idea: `Krokodýlí Zoo`, language: `cs` });
+
+            const { assignment } = await task.asPromise();
+
+            if (assignment.length === 0) {
+                throw new Error('Assignment is empty');
+            }
+        }).resolves.not.toThrow());
 });
 
 /**
